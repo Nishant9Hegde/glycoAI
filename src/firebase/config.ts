0bcc16d40e9da9e7e014962614cd7a543fbd6001
PARJANYA RAM <parjanya14@gmail.com>
2026-01-26 10:02:20 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-1044158505-cb03c",
+  "appId": "1:708525366261:web:5a4a705f792d0f80cc1ebc",
+  "apiKey": "AIzaSyAdQxy1H6WTdCpPXClErNCTO905oZWb3KQ",
+  "authDomain": "studio-1044158505-cb03c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "708525366261"
 };
